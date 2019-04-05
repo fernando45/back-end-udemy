8f96c79c83919eb02c0ddbcecde3603b024a217f
fernando45 <gestiona.fgonzalez@gmail.com>
2019-04-05 17:09:45 +0200

    usuario y token listo

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables.
 var app = express();
 
+// Body parser.
+
+// aplicación parse / x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 // Conexion a la base de datos.
 
+//const uri = "mongodb+srv://fernando:Matrix-45@cluster0-69mcn.mongodb.net/test?retryWrites=true";
+
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    //mongoose.connection.openUri(uri, (err, res) => {
     if (err) throw err;
     console.log(' Base de datos online: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
+// Importar rutas.
 
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones-
